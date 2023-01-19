896b3553bd6fb2f5970467f9b609dc8434d1ed50
Anindya Mitra <112335696+anindyamitra2002@users.noreply.github.com>
2023-01-19 18:36:36 +0530

    Update and rename threeSumClosest.java to ThreeSumClosest.java

--- Problems Solving/ThreeSumClosest.java
+++ Problems Solving/ThreeSumClosest.java
@@ -1,5 +1,5 @@
 import java.lang.Math;
-public class threeSumClosest {
+public class ThreeSumClosest {
     public static void main(String[] args) {
         int [] a= {-1,2,1,-4};
         int [][]c = new int [5][5];
